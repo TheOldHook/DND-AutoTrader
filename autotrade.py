8dde35056cd2bc0c4e3f6f37bd53d2081d7942b7
TheOldHook <71928222+TheOldHook@users.noreply.github.com>
2023-09-16 18:57:34 +0200

    Toggle images and multiple images

--- autotrade.py
+++ autotrade.py
@@ -1,30 +1,36 @@
+import os
 import pyautogui
 import time
 import tkinter as tk
 from tkinter import filedialog
-import threading  # Import the threading module
+from PIL import Image, ImageTk
+import threading
 
 # Global variables
-selected_item_image = None
+selected_item_images = {}  # Dictionary to keep track of selected items
 is_trading = False
+default_folder = './default_images/'  # Replace with the path to your default folder
 
 def start_trading():
-    global selected_item_image, is_trading
-    if selected_item_image is None:
-        print("No item selected for trading.")
+    global selected_item_images, is_trading
+
+    if not selected_item_images:
+        print("No items selected for trading.")
         return
 
-    print(f"Starting trading with {selected_item_image} in 5 seconds...")
+    print(f"Starting trading in 5 seconds...")
     time.sleep(5)
 
     is_trading = True
     while is_trading:
         chat_area = pyautogui.locateOnScreen('chat_area.jpg', confidence=0.8)
         if chat_area:
-            item_location = pyautogui.locateOnScreen(selected_item_image, confidence=0.8)
-            if item_location:
-                print(f"Found item: {selected_item_image}")
-                # Your drag and click logic here
+            for image_path, is_selected in selected_item_images.items():
+                if is_selected:
+                    item_location = pyautogui.locateOnScreen(image_path, confidence=0.8)
+                    if item_location:
+                        print(f"Found item: {image_path}")
+                        # Your drag and click logic here
         time.sleep(2)
 
 def stop_trading():
@@ -32,25 +38,41 @@ def stop_trading():
     is_trading = False
     print("Stopped trading.")
 
-def load_item_image():
-    global selected_item_image
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.png")])
-    if file_path:
-        selected_item_image = file_path
-        print(f"Selected item image: {file_path}")
+def toggle_image_selection(image_path, button):
+    global selected_item_images
+    is_selected = selected_item_images.get(image_path, False)
+    selected_item_images[image_path] = not is_selected
+    if not is_selected:
+        button.config(relief=tk.SUNKEN)  # Indicate the image is selected
+    else:
+        button.config(relief=tk.RAISED)  # Indicate the image is deselected
+    print(f"Toggled selection for {image_path}: {not is_selected}")
 
 # GUI setup
 root = tk.Tk()
-root.title("DND AutoTrader")
+root.title("Auto Trading Bot")
+root.geometry("600x400")  # Set window dimensions
 
-load_button = tk.Button(root, text="Load Item Image", command=load_item_image)
+load_button = tk.Button(root, text="Load Item Image", command=filedialog.askopenfilename)
 load_button.pack()
 
-# Start the start_trading function in a new thread
 start_button = tk.Button(root, text="Start Trading", command=lambda: threading.Thread(target=start_trading).start())
 start_button.pack()
 
 stop_button = tk.Button(root, text="Stop Trading", command=stop_trading)
 stop_button.pack()
+
+# Automatically show images from the default folder
+if os.path.exists(default_folder):
+    for filename in os.listdir(default_folder):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            img_path = os.path.join(default_folder, filename)
+            img = Image.open(img_path)
+            img = img.resize((50, 50), Image.LANCZOS)  # Changed to LANCZOS due to deprecation warning
+            img = ImageTk.PhotoImage(img)
+            img_button = tk.Button(root, image=img, relief=tk.RAISED)
+            img_button.image = img  # Keep a reference to prevent GC
+            img_button.pack(side=tk.LEFT)
+            img_button.config(command=lambda img_path=img_path, img_button=img_button: toggle_image_selection(img_path, img_button))  # Moved the command here
 
 root.mainloop()
