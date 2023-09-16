a4678b80c3097010d8d713f96b358f19280e8e8d
TheOldHook <71928222+TheOldHook@users.noreply.github.com>
2023-09-16 21:04:00 +0200

    Updates

--- autotrade.py
+++ autotrade.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import pyautogui
 import time
 import tkinter as tk
@@ -7,9 +8,12 @@ from PIL import Image, ImageTk
 import threading
 
 # Global variables
-selected_item_images = {}  # Dictionary to keep track of selected items
+selected_item_images = {}
 is_trading = False
-default_folder = './default_images/'  # Replace with the path to your default folder
+default_folder = './default_images/'
+
+if not os.path.exists(default_folder):
+    os.makedirs(default_folder)
 
 def start_trading():
     global selected_item_images, is_trading
@@ -47,32 +51,74 @@ def toggle_image_selection(image_path, button):
     else:
         button.config(relief=tk.RAISED)  # Indicate the image is deselected
     print(f"Toggled selection for {image_path}: {not is_selected}")
+    
+    
+
+def load_item_image():
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.png")])
+    if file_path:
+        shutil.copy(file_path, default_folder)
+        refresh_images()
+        
+def refresh_images():
+    global inner_frame
+    for widget in inner_frame.winfo_children():
+        widget.destroy()
+
+    row, col = 0, 0
+    for filename in os.listdir(default_folder):
+        if filename.endswith(('.jpg', '.png')):
+            img_path = os.path.join(default_folder, filename)
+            img = Image.open(img_path)
+            base_width = 100
+            w_percent = base_width / float(img.size[0])
+            h_size = int(float(img.size[1]) * float(w_percent))
+            img = img.resize((base_width, h_size), Image.LANCZOS)
+            img = ImageTk.PhotoImage(img)
+            
+            img_button = tk.Button(inner_frame, image=img, relief=tk.RAISED)
+            img_button.image = img
+            img_button.grid(row=row, column=col)
+            img_button.config(command=lambda img_path=img_path, img_button=img_button: toggle_image_selection(img_path, img_button))
+            
+            col += 1
+            if col > 3:  # 4 images per row
+                col = 0
+                row += 1
 
 # GUI setup
 root = tk.Tk()
 root.title("Auto Trading Bot")
-root.geometry("600x400")  # Set window dimensions
+root.geometry("600x400")
 
-load_button = tk.Button(root, text="Load Item Image", command=filedialog.askopenfilename)
-load_button.pack()
+frame = tk.Frame(root)
+frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+canvas = tk.Canvas(frame, width=450)  # Width adjusted to fit the grid
+canvas.pack(side=tk.LEFT)
+
+scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=canvas.yview)
+scrollbar.pack(side=tk.LEFT, fill=tk.Y)
+
+canvas.config(yscrollcommand=scrollbar.set)
+canvas.bind('<Configure>', lambda e: canvas.config(scrollregion=canvas.bbox('all')))
+
+inner_frame = tk.Frame(canvas)
+canvas.create_window((0, 0), window=inner_frame, anchor=tk.NW)
+
+load_button = tk.Button(root, text="Load Item Image", command=load_item_image)
+load_button.pack(pady=40)  # Moved down by 20px
 
 start_button = tk.Button(root, text="Start Trading", command=lambda: threading.Thread(target=start_trading).start())
-start_button.pack()
+start_button.pack(pady=5)  # 5px gap
 
 stop_button = tk.Button(root, text="Stop Trading", command=stop_trading)
-stop_button.pack()
+stop_button.pack(pady=5)  # 5px gap
 
-# Automatically show images from the default folder
-if os.path.exists(default_folder):
-    for filename in os.listdir(default_folder):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            img_path = os.path.join(default_folder, filename)
-            img = Image.open(img_path)
-            img = img.resize((50, 50), Image.LANCZOS)  # Changed to LANCZOS due to deprecation warning
-            img = ImageTk.PhotoImage(img)
-            img_button = tk.Button(root, image=img, relief=tk.RAISED)
-            img_button.image = img  # Keep a reference to prevent GC
-            img_button.pack(side=tk.LEFT)
-            img_button.config(command=lambda img_path=img_path, img_button=img_button: toggle_image_selection(img_path, img_button))  # Moved the command here
+refresh_images()
+
+inner_frame.update_idletasks()
+frame.config(width=inner_frame.winfo_width() + scrollbar.winfo_width(), height=inner_frame.winfo_height())
+canvas.config(scrollregion=canvas.bbox('all'))
 
 root.mainloop()
